92e65b93fa150ae063330b8ce85a9baeaf2cfc84
Yannik Könneker <y.koenneker@web.de>
2023-03-15 01:16:37 +0100

    init

---
+++ src/org/evaluator/Evaluator.java
@@ -0,0 +1,11 @@
+package org.evaluator;
+
+
+
+public class Evaluator {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/org/evaluator/Matcher.java
@@ -0,0 +1,65 @@
+package org.evaluator;
+
+import de.uni_mannheim.informatik.dws.winter.matching.MatchingEngine;
+import de.uni_mannheim.informatik.dws.winter.model.*;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.*;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.comparators.LabelComparatorJaccard;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+
+import org.SimilarityFlooding.Algorithms.*;
+import org.SimilarityFlooding.DataTypes.*;
+import org.SimilarityFlooding.SFConfig;
+import org.SimilarityFlooding.SimilarityFlooding;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Matcher {
+    public static List<AbsoluteSimilarity> matchSimilarityFlooding() {
+        Graph g1 = null;
+        Graph g2 = null;
+        SFConfig sfconfig = null;
+        var sf = new SimilarityFlooding(g1, g2, sfconfig);
+        sf.run(10, 0.05f);
+        var distances = sf.getDistances();
+        var graphs = sf.getGraphs();
+
+        var knowledge = Filter.Knowledge;
+        var ownKnowledge = new HashMap<>(knowledge);
+        ownKnowledge.put("first", "erster");
+        distances = Filter.knowledgeFilter(distances, ownKnowledge);
+        distances = Filter.typingConstraintFilter(graphs, distances);
+        distances = Filter.cardinalityConstraintFilter(distances);
+        distances = Selector.highestCumulativeSimilaritySelection(graphs, distances);
+
+        return distances;
+    }
+
+    public static List<?> matchWinter() {
+        DataSet<de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record, Attribute> data1 = new HashedDataSet<>();
+        DataSet<de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record, Attribute> data2 = new HashedDataSet<>();
+        try {
+            new CSVRecordReader(0).loadFromCSV(new File("scifi1.csv"), data1);
+            new CSVRecordReader(0).loadFromCSV(new File("scifi2.csv"), data2);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        MatchingEngine<de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record, Attribute> engine = new MatchingEngine<>();
+
+        Processable<Correspondence<Attribute, Attribute>> correspondences = null;
+        try {
+            correspondences = engine.runLabelBasedSchemaMatching(data1.getSchema(), data2.getSchema(), new LabelComparatorJaccard(), 0.5);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return correspondences.get().stream().toList();
+    }
+
+    public static List<?> matchXG() {
+        return new ArrayList<>();
+    }
+}
