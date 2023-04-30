abd1358caf5726174bdf68ffc286883e9d31dde9
Marina <marinakodenez@yahoo.de>
2023-05-01 01:31:54 +0200

    matcher hinzufügen

--- src/org/evaluator/Main.java
+++ src/org/evaluator/Main.java
@@ -1,6 +1,5 @@
 package org.evaluator;
 
-
 import edu.stanford.nlp.util.Quadruple;
 import edu.stanford.nlp.util.Triple;
 import org.converter.YAMLtoTruthCorrespondences;
@@ -16,81 +15,96 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length < 6) {
-            System.err.println("You need to pass at least one -alt and -o argument for the program to work.");
-            System.exit(1);
-        }
-
-        var alterationFiles = new ArrayList<Quintet<String, String, String, String, String>>();
-        var outfile = "";
-
-        // arguments are expected to be: truth Correspondences (YAML), truthYAML, truthCSV, alterationYAML, alterationCSV
-        for (var i = 0; i < args.length; i++) {
-            if (args[i].startsWith("-alt") && args.length > i + 5) {
-                alterationFiles.add(new Quintet<>(args[i + 1], args[i + 2], args[i + 3], args[i + 4], args[i + 5]));
-            }
-            if (args[i].startsWith("-o") && args.length > i + 1) {
-                outfile = args[i + 1];
-            }
-        }
-        assert !outfile.equals("");
-        BufferedWriter writer = null;
-        try {
-            writer = new BufferedWriter(new FileWriter(outfile));
-        } catch (IOException e) {
-            System.err.println("Outputfile could not be opened.");
-            System.exit(1);
-        }
-        var yaml = new Yaml();
-        var yamlWriter = new StringWriter();
-
-
-        AtomicInteger step = new AtomicInteger(1);
-        System.out.println("Starting analysis of " + alterationFiles.size() + "schema relations.\n" +
-                "This will take " + 3 * alterationFiles.size() + "steps.\n\n" +
-                "Currently at step " + step + "/" + 3 * alterationFiles.size());
-
-        for (Quintet<String, String, String, String, String> input : alterationFiles) {
-            var yamlTruth = input.getValue1();
-            var yamlAlteration = input.getValue3();
-            var csvTruth = input.getValue2().split(",");
-            var csvAlteration = input.getValue4().split(",");
-            var truthCorrespondences = YAMLtoTruthCorrespondences.convert(input.getValue0());
-
-            System.out.print("\rCurrently at step " + step.getAndIncrement() + "/" + 3 * alterationFiles.size());
-            var outSF = Matcher.matchSimilarityFlooding(yamlTruth, yamlAlteration);
-            var cmSF = CorrespondenceAnalyzer.Analyze(truthCorrespondences, outSF);
-            //writeToFile(yaml, writer, yamlWriter, csvTruth, alteration, outSF, cmSF);
-
-            System.out.print("\rCurrently at step " + step.getAndIncrement() + "/" + 3 * alterationFiles.size());
-            var outW = Matcher.matchWinter(csvTruth, csvAlteration);
-            var cmW = CorrespondenceAnalyzer.Analyze(truthCorrespondences, outW);
-            //writeToFile(yaml, writer, yamlWriter, truth, alteration, outW, cmW);
-
-            System.out.print("\rCurrently at step " + step.getAndIncrement() + "/" + 3 * alterationFiles.size());
-            var outXG = Matcher.matchXG(csvTruth, csvAlteration);
-            var cmXG = CorrespondenceAnalyzer.Analyze(truthCorrespondences, outXG);
-            //writeToFile(yaml, writer, yamlWriter, truth, alteration, outXG, cmXG);
-        }
-    }
-
-    public static void writeToFile(Yaml yaml,
-                                   BufferedWriter writer,
-                                   StringWriter yamlWriter,
-                                   String truth,
-                                   String alteration,
-                                   List<? extends Correspondence<?>> correspondences,
-                                   ConfusionMatrix confMatrix) {
-        yaml.dump(new Quadruple<>(
-                Arrays.stream(truth.split("/")).reduce((first, second) -> second).orElse(null),
-                Arrays.stream(alteration.split("/")).reduce((first, second) -> second).orElse(null),
-                correspondences,
-                confMatrix.Matrix()), yamlWriter);
-        try {
-            writer.append(yamlWriter.toString());
-        } catch (IOException e) {
-            System.err.println("Could not write to output file.");
-            System.exit(1);
-        }
+        /*
+         * * if (args.length < 6) {
+         * System.err.
+         * println("You need to pass at least one -alt and -o argument for the program to work."
+         * );
+         * System.exit(1);
+         * }
+         * 
+         * var alterationFiles = new ArrayList<Quintet<String, String, String, String,
+         * String>>();
+         * var outfile = "";
+         * 
+         * // arguments are expected to be: truth Correspondences (YAML), truthYAML,
+         * truthCSV, alterationYAML, alterationCSV
+         * for (var i = 0; i < args.length; i++) {
+         * if (args[i].startsWith("-alt") && args.length > i + 5) {
+         * alterationFiles.add(new Quintet<>(args[i + 1], args[i + 2], args[i + 3],
+         * args[i + 4], args[i + 5]));
+         * }
+         * if (args[i].startsWith("-o") && args.length > i + 1) {
+         * outfile = args[i + 1];
+         * }
+         * }
+         * assert !outfile.equals("");
+         * BufferedWriter writer = null;
+         * try {
+         * writer = new BufferedWriter(new FileWriter(outfile));
+         * } catch (IOException e) {
+         * System.err.println("Outputfile could not be opened.");
+         * System.exit(1);
+         * }
+         * var yaml = new Yaml();
+         * var yamlWriter = new StringWriter();
+         * 
+         * 
+         * AtomicInteger step = new AtomicInteger(1);
+         * System.out.println("Starting analysis of " + alterationFiles.size() +
+         * "schema relations.\n" +
+         * "This will take " + 3 * alterationFiles.size() + "steps.\n\n" +
+         * "Currently at step " + step + "/" + 3 * alterationFiles.size());
+         * 
+         * for (Quintet<String, String, String, String, String> input : alterationFiles)
+         * {
+         * var yamlTruth = input.getValue1();
+         * var yamlAlteration = input.getValue3();
+         * var csvTruth = input.getValue2().split(",");
+         * var csvAlteration = input.getValue4().split(",");
+         * var truthCorrespondences =
+         * YAMLtoTruthCorrespondences.convert(input.getValue0());
+         * 
+         * System.out.print("\rCurrently at step " + step.getAndIncrement() + "/" + 3 *
+         * alterationFiles.size());
+         * var outSF = Matcher.matchSimilarityFlooding(yamlTruth, yamlAlteration);
+         * var cmSF = CorrespondenceAnalyzer.Analyze(truthCorrespondences, outSF);
+         * //writeToFile(yaml, writer, yamlWriter, csvTruth, alteration, outSF, cmSF);
+         * 
+         * System.out.print("\rCurrently at step " + step.getAndIncrement() + "/" + 3 *
+         * alterationFiles.size());
+         * var outW = Matcher.matchWinter(csvTruth, csvAlteration);
+         * var cmW = CorrespondenceAnalyzer.Analyze(truthCorrespondences, outW);
+         * //writeToFile(yaml, writer, yamlWriter, truth, alteration, outW, cmW);
+         * 
+         * System.out.print("\rCurrently at step " + step.getAndIncrement() + "/" + 3 *
+         * alterationFiles.size());
+         * var outXG = Matcher.matchXG(csvTruth, csvAlteration);
+         * var cmXG = CorrespondenceAnalyzer.Analyze(truthCorrespondences, outXG);
+         * //writeToFile(yaml, writer, yamlWriter, truth, alteration, outXG, cmXG);
+         * }
+         * }
+         * 
+         * public static void writeToFile(Yaml yaml,
+         * BufferedWriter writer,
+         * StringWriter yamlWriter,
+         * String truth,
+         * String alteration,
+         * List<? extends Correspondence<?>> correspondences,
+         * ConfusionMatrix confMatrix) {
+         * yaml.dump(new Quadruple<>(
+         * Arrays.stream(truth.split("/")).reduce((first, second) ->
+         * second).orElse(null),
+         * Arrays.stream(alteration.split("/")).reduce((first, second) ->
+         * second).orElse(null),
+         * correspondences,
+         * confMatrix.Matrix()), yamlWriter);
+         * try {
+         * writer.append(yamlWriter.toString());
+         * } catch (IOException e) {
+         * System.err.println("Could not write to output file.");
+         * System.exit(1);
+         * }
+         */
     }
 }
--- src/org/evaluator/Matcher.java
+++ src/org/evaluator/Matcher.java
@@ -1,10 +1,13 @@
 package org.evaluator;
 
 import de.uni_mannheim.informatik.dws.winter.matching.MatchingEngine;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.BlockingKeyIndexer.VectorCreationMethod;
 import de.uni_mannheim.informatik.dws.winter.model.*;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.*;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.blocking.DefaultAttributeValuesAsBlockingKeyGenerator;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.comparators.LabelComparatorJaccard;
 import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+import de.uni_mannheim.informatik.dws.winter.similarity.vectorspace.VectorSpaceMaximumOfContainmentSimilarity;
 
 import org.SimilarityFlooding.Algorithms.*;
 import org.SimilarityFlooding.DataTypes.*;
@@ -44,38 +47,164 @@ public class Matcher {
         return distances;
     }
 
-    public static List<org.utils.Correspondence<String>> matchWinter(String[] truth, String[] alternation) {
+    // DUPLIKATBASIERT
+    // load data
+
+    private static final Logger logger = WinterLogManager.activateLogger("default");
+
+    public static List<org.utils.Correspondence<String>> matchWinterDuplicate(String[] truth, String[] alternation) {
         DataSet<de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record, Attribute> data1 = new HashedDataSet<>();
+        try {
+            new CSVRecordReader(0).loadFromCSV(new File(
+                    "D:\\Uni\\5\\Projekt\\roject_evaluator\\src\\org\\evaluator\\legalacts1.csv"),
+                    data1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         DataSet<de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record, Attribute> data2 = new HashedDataSet<>();
-        Arrays.stream(truth).forEach(file -> {
-            try {
-                new CSVRecordReader(0).loadFromCSV(new File(file), data1);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        try {
+            new CSVRecordReader(0).loadFromCSV(new File(
+                    "D:\\Uni\\5\\Projekt\\roject_evaluator\\src\\org\\evaluator\\legalacts2.csv"),
+                    data2);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // load duplicates
+        Processable<Correspondence<de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record, Attribute>> duplicates = Correspondence
+                .loadFromCsv(new File(
+                        "D:\\Uni\\5\\Projekt\\roject_evaluator\\src\\org\\evaluator\\legalacts_correspondences.csv"),
+                        data1, data2);
+
+        // define the schema matching rule
+        VotingMatchingRule<Attribute, de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record> schemaRule = new VotingMatchingRule<Attribute, Record>(
+                1.0) {
+
+            private static final long serialVersionUID = 1L;
+
+            public double compare(Attribute a1, Attribute a2,
+                    Correspondence<Record, Matchable> c) {
+                // get both attribute values
+                String value1 = c.getFirstRecord().getValue(a1);
+                String value2 = c.getSecondRecord().getValue(a2);
+
+                // check if they are equal
+                if (value1 != null && value2 != null && !value1.equals("0.0") && value1.equals(value2)) {
+                    return 1.0;
+                } else {
+                    return 0.0;
+                }
             }
-        });
-        Arrays.stream(alternation).forEach(file -> {
-            try {
-                new CSVRecordReader(0).loadFromCSV(new File(file), data1);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        };
+
+        // initialize matching engine
+        MatchingEngine<de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record, Attribute> engine = new MatchingEngine<>();
+
+        // execute the matching
+        Processable<Correspondence<Attribute, de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record>> correspondences = null;
+        try {
+            correspondences = engine.runDuplicateBasedSchemaMatching(data1.getSchema(), data2.getSchema(), duplicates,
+                    (VotingMatchingRule<Attribute, Record>) schemaRule, null, new VotingAggregator<>(true, 1.0),
+                    new NoSchemaBlocker<>());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        // print results (fragen, wie genau das funtioniert)
+        return correspondences.get().stream().map(c -> new org.utils.Correspondence<>(
+                c.getFirstRecord().getName(),
+                c.getSecondRecord().getName(),
+                c.getSimilarityScore())).toList();
+    }
+
+    // INSTANZBASIERT
+    public static List<org.utils.Correspondence<String>> matchWinterInstance(String[] truth, String[] alternation) {
+        DataSet<de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record, Attribute> data1 = new HashedDataSet<>();
+        try {
+            new CSVRecordReader(-1).loadFromCSV(new File(
+                    "D:\\Uni\\5\\Projekt\\roject_evaluator\\src\\org\\evaluator\\legalacts1.csv"),
+                    data1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        DataSet<de.uni_mannheim.informatik.dws.winter.model.defultmodel.Record, Attribute> data2 = new HashedDataSet<>();
+        try {
+            new CSVRecordReader(-1).loadFromCSV(new File(
+                    "D:\\Uni\\5\\Projekt\\roject_evaluator\\src\\org\\evaluator\\legalacts2.csv"),
+                    data2);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // define a blocker that uses the attribute values to generate pairs
+        InstanceBasedSchemaBlocker<de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record, Attribute> blocker = new InstanceBasedSchemaBlocker<>(
+                new DefaultAttributeValueGenerator(data1.getSchema()),
+                new DefaultAttributeValueGenerator(data2.getSchema()));
+
+        // to calculate the similarity score, aggregate the pairs by counting
+        // and normalise with the number of record in the smaller dataset
+        // (= the maximum number of records that can match)
+        VotingAggregator<Attribute, MatchableValue> aggregator = new VotingAggregator<>(
+                false,
+                Math.min(data1.size(), data2.size()),
+                0.0);
+
+        // initialize matching engine
+        MatchingEngine<de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record, Attribute> engine = new MatchingEngine<>();
+
+        // run the matching engine
+        Processable<Correspondence<Attribute, MatchableValue>> correspondences = null;
+        try {
+            correspondences = engine.runInstanceBasedSchemaMatching(data1, data2,
+                    new DefaultAttributeValuesAsBlockingKeyGenerator(data1.getSchema()),
+                    new DefaultAttributeValuesAsBlockingKeyGenerator(data2.getSchema()),
+                    VectorCreationMethod.TermFrequencies,
+                    new VectorSpaceMaximumOfContainmentSimilarity(), 0.0);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        // print results (fragen, wie genau das funtioniert)
+        return correspondences.get().stream().map(c -> new org.utils.Correspondence<>(
+                c.getFirstRecord().getName(),
+                c.getSecondRecord().getName(),
+                c.getSimilarityScore())).toList();
+    }
+
+    // LABELBASIERT
+    public static List<org.utils.Correspondence<String>> matchWinterLabel(String[] truth, String[] alternation) {
+        DataSet<de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record, Attribute> data1 = new HashedDataSet<>();
+        try {
+            new CSVRecordReader(0).loadFromCSV(new File(
+                    "D:\\Uni\\5\\Projekt\\roject_evaluator\\src\\org\\evaluator\\legalacts1.csv"),
+                    data1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        DataSet<de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record, Attribute> data2 = new HashedDataSet<>();
+        try {
+            new CSVRecordReader(0).loadFromCSV(new File(
+                    "D:\\Uni\\5\\Projekt\\roject_evaluator\\src\\org\\evaluator\\legalacts2.csv"),
+                    data1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         MatchingEngine<de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Record, Attribute> engine = new MatchingEngine<>();
 
         Processable<Correspondence<Attribute, Attribute>> correspondences = null;
         try {
-            correspondences = engine.runLabelBasedSchemaMatching(data1.getSchema(), data2.getSchema(), new LabelComparatorJaccard(), 0.5);
+            correspondences = engine.runLabelBasedSchemaMatching(data1.getSchema(), data2.getSchema(),
+                    new LabelComparatorJaccard(), 0.5);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-        return correspondences.get().stream().map(c ->
-                new org.utils.Correspondence<>(
-                        c.getFirstRecord().getName(),
-                        c.getSecondRecord().getName(),
-                        c.getSimilarityScore())).toList();
+        return correspondences.get().stream().map(c -> new org.utils.Correspondence<>(
+                c.getFirstRecord().getName(),
+                c.getSecondRecord().getName(),
+                c.getSimilarityScore())).toList();
     }
 
     public static List<org.utils.Correspondence<String>> matchXG(String[] truth, String[] alternation) {
